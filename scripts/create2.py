dcd017c11a24ee245e3005d58abffbf300e1f8f6
karmacoma <karmacoma.eth@gmail.com>
2022-05-12 16:07:59 -0700

    Make scripts/create2.py properly executable

--- scripts/create2.py
+++ scripts/create2.py
@@ -1,10 +1,12 @@
+#!/usr/bin/env python3
+
 import random
 import sys
 from web3 import Web3
 
 def _create2(deployer, salt_hexstr, hashed_bytecode):
     addr_hexbytes = Web3.keccak(hexstr=('ff' + deployer + salt_hexstr + hashed_bytecode))
-    addr = Web3.toHex(addr_hexbytes)[-40:] 
+    addr = Web3.toHex(addr_hexbytes)[-40:]
     return addr
 
 # expecting deployer='aabbccdd' (20 bytes -> 40 characters)
@@ -27,7 +29,7 @@ def create2_search(deployer, predicate, bytecode):
         addr = _create2(deployer, salt_hexstr, hashed_bytecode)
         if salt % 1000 == 0:
             print('.', end='', flush=True)
-            
+
         if predicate(addr):
             print(f"\nFound a match after {salt} attempts: {addr}")
             break
@@ -39,25 +41,25 @@ def main():
         print()
         print(f"When passing a salt value, this script prints the address of the newly deployed contract based on the deployer address and bytecode hash.")
         print(f"Example: python3 {sys.argv[0]} Bf6cE3350513EfDcC0d5bd5413F1dE53D0E4f9aE 42 602a60205260206020f3")
-        print()        
+        print()
         print(f"When passing a predicate, this script will search for a salt value such that the new address satisfies the predicate.")
         print(f"Example: python3 {sys.argv[0]} Bf6cE3350513EfDcC0d5bd5413F1dE53D0E4f9aE 'lambda addr: \"badc0de\" in addr.lower()' 602a60205260206020f3")
         print(f"Another predicate that may be useful: 'lambda addr: addr.startswith(\"0\" * 8)' 602a60205260206020f3")
         sys.exit(0)
-    
+
     deployer_addr = sys.argv[1]
     if deployer_addr.startswith('0x'):
         deployer_addr = deployer_addr[2:]
-    
+
     bytecode = sys.argv[3]
-    
+
     try:
         salt = int(sys.argv[2])
         print(create2(deployer_addr, salt, bytecode))
     except ValueError:
         predicate = eval(sys.argv[2])
         create2_search(deployer_addr, predicate, bytecode)
-            
+
 
 if __name__ == '__main__':
     main()
