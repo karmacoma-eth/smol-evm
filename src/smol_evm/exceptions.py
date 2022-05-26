13f542793dccf8acc69a0055afe707c56691a2bf
karmacoma <karmacoma.eth@gmail.com>
2022-05-26 16:26:47 -0700

    Add more helpful exception descriptions

--- src/smol_evm/exceptions.py
+++ src/smol_evm/exceptions.py
@@ -8,14 +8,23 @@ class EVMException(Exception):
     context: ExecutionContext
 
 
+@dataclass
 class UnknownOpcode(EVMException):
-    ...
+    opcode: int
+
+    def __str__(self):
+        return f"opcode={hex(self.opcode)}, context={self.context}"
 
 
 class InvalidCodeOffset(EVMException):
-    ...
+    offset: int
+
+    def __str__(self):
+        return f"offset={hex(self.opcode)}, context={self.context}"
 
 
-@dataclass
 class InvalidJumpDestination(EVMException):
     target_pc: int
+
+    def __str__(self):
+        return f"target_pc={hex(self.opcode)}, context={self.context}"
