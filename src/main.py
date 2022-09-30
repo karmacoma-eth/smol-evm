c4daa89e0fc99c5d6fd8487ee867db674cfc8b61
karmacoma <karmacoma.eth@gmail.com>
2022-09-29 17:22:57 -0700

    Invite contributions on UnknownOpcode

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@
 import argparse
 
 from smol_evm.context import Calldata
+from smol_evm.opcodes import UnknownOpcode
 from smol_evm.runner import run
 
 
@@ -22,8 +23,26 @@ def main():
     code = bytes.fromhex(strip_0x(args.code))
     calldata = bytes.fromhex(strip_0x(args.calldata)) if args.calldata else bytes()
 
-    ret = run(code=code, calldata=calldata, verbose=True, print_stack=True, print_memory=True)
-    print(f"0x{ret.hex()}")
+    try:
+        ret = run(
+            code=code,
+            calldata=calldata,
+            verbose=True,
+            print_stack=True,
+            print_memory=True,
+        )
+        print(f"0x{ret.hex()}")
+    except UnknownOpcode as e:
+        print(f"🚧 Encountered an unknown/unimplemented opcode: {hex(e.opcode)}")
+        print(f"If you want to contribute to smol🤏evm, now is your chance!")
+        print()
+        print(f"    🕵️‍♂️  Look up the opcode: https://ethereum.github.io/yellowpaper")
+        print(f"    🧰 Implement it in src/smol_evm/opcodes.py")
+        print(f"    👨‍💻 Create some unit tests for it")
+        print(f"    💣 Run the tests: `poetry run pytest -v`")
+        print(f"    🏗️  Create a pull request on GitHub")
+        print(f"    🥳 Congrats, you're a contributor!")
+        print()
 
 
 if __name__ == "__main__":
