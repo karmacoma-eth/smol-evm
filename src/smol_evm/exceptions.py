6d7eb229771c0ba20455b8f9c946483987b58ded
karmacoma <karmacoma.eth@gmail.com>
2023-01-12 16:38:30 -0800

    Fix some internal exceptions

--- src/smol_evm/exceptions.py
+++ src/smol_evm/exceptions.py
@@ -21,7 +21,7 @@ class InvalidCodeOffset(EVMException):
     offset: int
 
     def __str__(self):
-        return f"offset={hex(self.opcode)}, context={self.context}"
+        return f"offset={hex(self.offset)}, context={self.context}"
 
 
 @dataclass
@@ -29,4 +29,4 @@ class InvalidJumpDestination(EVMException):
     target_pc: int
 
     def __str__(self):
-        return f"target_pc={hex(self.opcode)}, context={self.context}"
+        return f"target_pc={hex(self.target_pc)}, context={self.context}"
