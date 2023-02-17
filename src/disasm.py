9972b720cb93c49769d26619e3f292bd24e98c5e
karmacoma <karmacoma.eth@gmail.com>
2023-02-17 13:26:16 -0800

    Improve DataSection ergonomics

--- src/disasm.py
+++ src/disasm.py
@@ -18,18 +18,30 @@ def strip_0x(s: str):
 
 @dataclass
 class DataSection:
-    start_pc: int
+    start_pc: int = -1
     data: bytes = b""
 
-    def render(self, output: List[str]):
+    def flush(self, output: List[str]):
+        if self.is_empty():
+            return
+
         data_format_str = f"{{:04x}}: DATA 0x{{:0{len(self.data) * 2}x}}"
         output.append(data_format_str.format(self.start_pc, int.from_bytes(self.data, byteorder="big")))
+
+        self.data = b""
+        self.start_pc = -1
+
+    def is_empty(self):
+        return len(self.data) == 0
+
+    def initialize(self, pc: int):
+        self.start_pc = pc
 
 
 def disassemble(code: bytes) -> Sequence[str]:
     output = []
     reading_code = True
-    data_section = None
+    data_section = DataSection()
 
     context = ExecutionContext(code=code)
 
@@ -45,10 +57,7 @@ def disassemble(code: bytes) -> Sequence[str]:
         reading_code = reading_code or insn.opcode is JUMPDEST.opcode
 
         if reading_code:
-            if data_section is not None:
-                data_section.render(output)
-                data_section = None
-
+            data_section.flush(output)
             if insn.is_push() and len(push_data) < insn.push_width():
                 # make sure we handle truncated PUSH arguments
                 output.append(f"{pc_str}: PUSH{insn.push_width()} 0x{push_data.hex()} # truncated")
@@ -60,15 +69,13 @@ def disassemble(code: bytes) -> Sequence[str]:
         else:
             # just like the algorithm for valid jump destination validation,
             # we parse PUSH instructions and skip their arguments (so no JUMPDESTs can hide there)
-            if data_section is None:
-                data_section = DataSection(original_pc)
+            if data_section.is_empty():
+                data_section.initialize(original_pc)
 
             data_section.data += insn.opcode.to_bytes(1, byteorder="big")
             data_section.data += push_data
 
-    if data_section is not None:
-        data_section.render(output)
-
+    data_section.flush(output)
     return output
 
 
